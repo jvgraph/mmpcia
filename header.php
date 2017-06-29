cdd0440ad8d39ef1e796a2d40e730688c9b79fb5
Juan Vargas <vjuan23@gmail.com>
2017-06-29 15:04:38 -0400

    initialized npm and added nav.scss

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Bitter:700|Montserrat" rel="stylesheet">
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <div class="header">
+            <div class="header__logo">
+                <?php include_once('includes/logo.php');?>
+            </div>
+            <div class="header__nav">
+            </div>
+        </div>
--- index.php
+++ index.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://fonts.googleapis.com/css?family=Bitter:700|Montserrat" rel="stylesheet">
-    </head>
-    <body>
-        <div class="header">
-            <div class="header__logo">
-                <?php include_once('includes/logo.php');?>
-            </div>
-            <div class="header__nav">
-            </div>
-        </div>
+<?php get_header(); ?>
+
+        <div class="content">
             <?php
                 if(have_posts()): while(have_posts()): the_post();
                     the_title();
                 endwhile;
                 endif;
             ?>
-        <div class="content">
         </div>
-        <footer class="footer">
-            
-        </footer>
-    </body>
-</html>
+<?php get_footer(); ?>

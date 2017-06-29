4dd71bc2dfa7439c5104d301ec8a4f93c8f66fb9
Juan Vargas <vjuan23@gmail.com>
2017-06-29 14:52:18 -0400

    adding sass and theme organization

--- index.php
+++ index.php
@@ -4,16 +4,22 @@
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bitter:700|Montserrat" rel="stylesheet">
     </head>
     <body>
         <div class="header">
             <div class="header__logo">
-                <img src="" alt="MMPCIA">
+                <?php include_once('includes/logo.php');?>
             </div>
             <div class="header__nav">
             </div>
         </div>
+            <?php
+                if(have_posts()): while(have_posts()): the_post();
+                    the_title();
+                endwhile;
+                endif;
+            ?>
         <div class="content">
         </div>
         <footer class="footer">

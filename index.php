5eb59d5718dbf7a2678e4d979ebd34e41d0c9c8a
Juan Vargas <vjuan23@gmail.com>
2017-06-29 12:57:05 -0400

    init files theme

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="header">
+            <div class="header__logo">
+                <img src="" alt="MMPCIA">
+            </div>
+            <div class="header__nav">
+            </div>
+        </div>
+        <div class="content">
+        </div>
+        <footer class="footer">
+            
+        </footer>
+    </body>
+</html>

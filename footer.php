af5eb61fdab76ab909cad684bcf20661fd1742f2
Juan Vargas <vjuan23@gmail.com>
2017-07-02 20:26:57 -0400

    added boostrap

--- footer.php
+++ footer.php
@@ -1,37 +1,12 @@
-
-		<footer id="colophon" class="site-footer" role="contentinfo">
-
-
-			<?php if ( has_nav_menu( 'social' ) ) : ?>
-				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'social',
-							'menu_class'     => 'social-links-menu',
-							'depth'          => 1,
-							'link_before'    => '<span class="screen-reader-text">',
-							'link_after'     => '</span>',
-						) );
-					?>
-				</nav><!-- .social-navigation -->
-			<?php endif; ?>
-
-			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the twentysixteen footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'twentysixteen_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
-		</footer><!-- .site-footer -->
-	</div><!-- .site-inner -->
-</div><!-- .site -->
-
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="site-info">
+                &copy; mmpcia
+            </div>
+        </div>
+    </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <?php
 get_header(); ?>
 <div class="container" id="main">
+
+	<div class="row">
+
 			<div class="article">
 				<?php
 					if(have_posts()): while(have_posts()): the_post();
 				?>
 				<div class="article__post">
-				<h1 class="article__title">
-					<?php the_title(); ?>
-				</h1>
-				<div class="article__content">
-					<?php the_content(); ?>
-				</div>
+					<h1 class="article__title">
+						<?php the_title(); ?>
+					</h1>
+					<div class="article__content">
+						<?php the_content(); ?>
+					</div>
 				</div>
 				<?php      
 					endwhile;
 					endif;
 				?>
 			</div>
-	<?php get_sidebar(); ?>
+			<?php get_sidebar(); ?>
+	</div>
 </div>
 <?php get_footer(); ?>

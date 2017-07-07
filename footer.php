8ab672446f88230ed33af40e59ff234e08b86925
Juan Vargas <vjuan23@gmail.com>
2017-07-07 11:07:39 -0400

    deleted boostrap and complete the css/scss for the home page

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
+
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+
 <div id="mega-footer" class="container">
     <div class="row">
         <ul>
@@ -9,7 +25,7 @@
                 <p>
                 PO BOX 3270 New York 10027 <br>
                 P: 212.369.4241 <br>
-                E: info@mmpcia.org
+                E: <a href="mailto:info@mmpcia.org">info@mmpcia.org</a>
                 </p>
             </li>
             <li class="mega-item">
@@ -38,6 +54,27 @@
         </ul>
     </div>
 </div>
-<?php
- require_once( get_template_directory() . '/footer.php' );
-?>
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			
+			<div class="site-info">
+				<?php
+					/**
+					 * Fires before the twentysixteen footer text for footer customization.
+					 *
+					 * @since Twenty Sixteen 1.0
+					 */
+					do_action( 'twentysixteen_credits' );
+				?>
+				<span class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</span>
+			</div><!-- .site-info -->
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>

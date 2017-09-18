b188340ce154abf4377655805cd3973fd8db5e26
Juan Vargas <vjuan23@gmail.com>
2017-09-17 22:55:00 -0400

    district page

--- partials/featured.php
+++ partials/featured.php
@@ -1,19 +1,28 @@
 <div class="featured-events">
 	<div class="row">
+	<?php 
+	$original_query = $wp_query;
+	$wp_query = null;
+	$args = array(
+		'posts_per_page'=>5, 
+		'tag' => 'feature'
+	);
+	$wp_query = new WP_Query( $args );
+	?>
 		<?php 
-				if( have_rows('contribute', 'option') ):     
-				while( have_rows('contribute', 'option') ): the_row(); ?>
-			<div class="half">
-					<?php the_field('contribute_image'); ?>
-					<img src="<?php the_sub_field('contribute_image'); ?>" alt="">
-					<h2>
-							<a href="<?php the_sub_field('contribute_link'); ?>">
-								<?php the_sub_field('title'); ?>
-							</a>
-					</h2>
-			</div>
+			if( have_rows( 'contribute', 'option' ) ):     
+				while( have_rows( 'contribute', 'option' ) ): the_row(); ?>
+		<div class="half">
+			<?php the_field('contribute_image'); ?>
+			<img src="<?php the_sub_field('contribute_image'); ?>" alt="">
+			<h2>
+				<a href="<?php the_sub_field('contribute_link'); ?>">
+					<?php the_sub_field('title'); ?>
+				</a>
+			</h2>
+		</div>
 		<?php 
-				endwhile;
+		endwhile;
 		endif; ?>
 	</div>
 </div>
--- partials/home-cat.php
+++ partials/home-cat.php
@@ -5,51 +5,46 @@
 
 <?php
 $cat_name = array('education','real-state','recreation');
-
 $do_not_duplicate = array();
-
 $categories = get_categories(
-    array(
-        'include'=> '24, 27, 28'
-    )
+		array(
+				'include'=> '24, 27, 28'
+		)
 );
 $do_not_duplicate = array();
 
 foreach ( $categories as $category ) {
-    $args = array(
-        'cat' => $category->term_id,
-        'orderby' =>'post_date',
-        'order' => 'DESC',
-        'post_type' => 'post',
-        'posts_per_page' => '1',
+	$args = array(
+		'cat' => $category->term_id,
+		'orderby' =>'post_date',
+		'order' => 'DESC',
+		'post_type' => 'post',
+		'posts_per_page' => '1',
+	);
+		$query = new WP_Query( $args );
 
-    );
-    
-    $query = new WP_Query( $args );
+		if ( $query->have_posts() ) {
+				$posttype = get_post_type(get_the_ID());?>
+					<?php while ( $query->have_posts() ) {
+						$query->the_post();
+						$do_not_duplicate[] = $post->ID;
+					?>
+						<div class="community-item">
+							<?php 
+							if(has_post_thumbnail()) { 
+								the_post_thumbnail();
+							} ?>
+							<h2>
+								<a href="<?php echo get_category_link($category->term_id); ?>">
+									<?php echo $category->name; ?>
+								</a>
+							</h2>
+						</div>							
+				<?php } // end while ?>
+		<?php } // end if
 
-    if ( $query->have_posts() ) {
-        $posttype = get_post_type(get_the_ID());?>
-            <?php while ( $query->have_posts() ) {
-
-                $query->the_post();
-                $do_not_duplicate[] = $post->ID;
-                ?>
-                    <div class="community-item">
-                        <?php if ( has_post_thumbnail() ) { 
-                                the_post_thumbnail( );
-                        } ?>
-                            <h2>
-                                <a href="<?php echo get_category_link($category->term_id); ?>">
-                                    <?php echo $category->name; ?>
-                                </a>
-                            </h2>
-                    </div>
-                            
-        <?php } // end while ?>
-    <?php } // end if
-
-    // Use reset to restore original query.
-    wp_reset_postdata();
+		// Use reset to restore original query.
+		wp_reset_postdata();
 }
 
 ?>        

19c362ca67889c90fcf6601c0cecab01c10553d3
Juan Vargas <vjuan23@gmail.com>
2017-09-23 18:14:13 -0400

    home page feature block content

--- get-invlove.php
+++ get-invlove.php
@@ -1,42 +1,20 @@
 <?php
   /*
-  Template Name: Get Involve
+  Template Name: Featured Area
   */
   get_header();
 ?>
-<?php 
-
-  $args = array(
-    'posts_per_page'=>6, 
-    'category__in' => array(17, 9, 18, 13, 12)
-  );
-  $community = new WP_Query( $args );
-?>
 <div id="primary" class="content-area community-page">
-  <?php
-  if( $community->have_posts() ) : ?>
-    <?php the_title('<h1 class="page-title">', '</h1>', true); ?>
-    <div class="community">
-      <?php while ( $community->have_posts() ) : $community->the_post(); ?>
-      <article>
-        <?php if( has_post_thumbnail() ){ ?>
-        <div class="post-image">
-          <?php 
-            the_post_thumbnail(); 
-          ?>
-        </div>
-        <?php } ?>
-        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-        <?php the_excerpt() ?>
-        <footer>
-           Post in Category : <?php the_category( ' ' ); ?> | Posted on: <?php the_date(); ?>
-        </footer>
-      </article>
-      <?php endwhile; ?>
-    </div>
-  <?php
-endif;
-  ?>
+<?php 
+ if(is_page(5012)){
+  
+  include 'partials/get-involve.php';
+  
+}else if(is_page(5014)){
+  include 'partials/community.php';
+}
+
+?>
 </div>
 
 <?php 
---
+++ partials/get-involve.php
@@ -0,0 +1,33 @@
+<?php 
+  $page_args = array(
+    'posts_per_page'=>6, 
+    'category__in' => array(4, 9, 18,)
+  );
+  $community = new WP_Query( $page_args );
+
+
+?>
+  <?php
+  if( $community->have_posts() ) : ?>
+    <?php the_title('<h1 class="page-title">', '</h1>', true); ?>
+    <div class="community">
+      <?php while ( $community->have_posts() ) : $community->the_post(); ?>
+      <article>
+        <?php if( has_post_thumbnail() ){ ?>
+        <div class="post-image">
+          <?php 
+            the_post_thumbnail(); 
+          ?>
+        </div>
+        <?php } ?>
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+        <?php the_excerpt() ?>
+        <footer>
+           Post in Category : <?php the_category( ' ' ); ?> | Posted on: <?php the_date(); ?>
+        </footer>
+      </article>
+      <?php endwhile; ?>
+    </div>
+  <?php
+endif;
+  ?>
